8f65174a53d9f3eeecbed95caea318dc08323b76
Sonal Chindage <sonalchindage@gmail.com>
2023-09-24 12:30:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';
+
+
 import './App.css';
+import NoteContainer from './Components/NoteContainer/NoteContainer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <NoteContainer/>
+       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +22,65 @@ function App() {
           Learn React
         </a>
       </header>
+    </div>
+  );
+}
+
+export default App;*/
+
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import NoteContainer from "./Components/NoteContainer/NoteContainer";
+import Sidebar from "./Components/Sidebar/Sidebar";
+
+function App() {
+  const [notes,setNotes]=useState (
+    JSON.parse(localStorage.getItem('notes-app')) ||[]
+    );
+
+const addNote=(colour)=>{
+const tempNotes=[...notes];
+
+tempNotes.push({
+  id:Date.now() +""+Math.floor(Math.random()*78),
+  text:"",
+  time: Date.now(),
+  colour,
+});
+setNotes(tempNotes);
+};
+
+const deleteNote=(id)=>{
+  const tempNotes=[...notes];
+
+  const index=tempNotes.findIndex((item)=>item.id===id);
+  if(index<0) return;
+  
+  tempNotes.splice(index,1);
+  setNotes(tempNotes);
+};
+
+const updateText=( text,id)=>{
+  const tempNotes=[...notes];
+
+const index=tempNotes.findIndex((item)=>item.id===id);
+  if(index<0) return;
+  
+tempNotes[index].text=text;
+setNotes(tempNotes);
+};
+
+useEffect(()=>{
+localStorage.setItem('notes-app',JSON.stringify(notes))
+},[notes])
+
+
+  return (
+    <div className="App">
+      <Sidebar addNote={addNote}/>
+      <NoteContainer notes={notes} deleteNote={deleteNote}
+      updateText={updateText}
+      />
     </div>
   );
 }
